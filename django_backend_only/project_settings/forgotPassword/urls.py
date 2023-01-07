8af54718bd43cd7036be74344fa87200eeb89aca
Keshawn Strong <keshawnstrong@MyLaptop.hsd1.ca.comcast.net>
2023-01-07 01:03:06 -0500

    application not functioning

--- django_backend_only/project_settings/forgotPassword/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from forgotPassword.views import recover, recover_link
-
-urlpatterns = [
-    path('', recover, name='forgotpassword-home'),
-    path('forgot_password_activation/', recover_link, name="recover-password")
-]
--- django_backend_only/project_settings/project_settings/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-logging.basicConfig(filename="app.log", format="%(asctime)s %(message)s", filemode='a')
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
